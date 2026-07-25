0559ed5cd66b53b8a5a71b08c51d2c5eb25f6193
Amar Prakash <prakashamar730@gmail.com>
2026-07-25 17:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function stringChop(str, size) {
   // your code here
+	let ans = [];
+	let count = 0;
+	let s = "";
+	for(let i = 0; i < str.length; i++) {
+		count++;
+		if(count < 5) {
+			s += str[i];
+		} else {
+			count = 0;
+			ans.push(s);
+			s = "";
+		}
+	}
+	if(s.length > 0) {
+		ans.push(s);
+	}
+	return ans;
 }
 
 // Do not change the code below
